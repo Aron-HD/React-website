f3b6ecee631ee140bf02cdb2a6b49cc350fba7f1
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-15 14:16:08 +0000

    replaced anchors with react Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Aron Hayes Davidson</h1>
+      <Link to="/" className="item">
+        <h1>Aron Hayes Davidson</h1>
+      </Link>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/create">New blog</a>
+        <Link to="/about" className="item">
+          About
+        </Link>
+        <Link to="/contact" className="item">
+          Contact
+        </Link>
+        <Link to="/create" className="item">
+          New blog
+        </Link>
       </div>
     </nav>
   );
