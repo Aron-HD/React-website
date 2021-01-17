be3db629bf9d63385b18e79c9462c42ce58db175
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-17 16:28:37 +0000

    added fontawsome social icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,29 @@
-// import { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
     <div className="contact">
       <h2>Contact</h2>
-      <p>Please do get in touch</p>
       <ul type="none">
         <li>
-          <a href="mailto:aronhdavidson@gmail.com">aronhdavidson@gmail.com</a>
+          <a href="mailto:aronhdavidson@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+            aronhdavidson@gmail.com
+          </a>
         </li>
         <li>
-          <a href="https://github.com/Aron-HD">Github</a>
+          <a href="https://github.com/aron-hd">
+            <FontAwesomeIcon icon={faGithub} />
+            Github
+          </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/aron-hd/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/aron-hd/">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+            LinkedIn
+          </a>
         </li>
       </ul>
     </div>
