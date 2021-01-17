c0780bea7ba6e00464e1d3f922f40773325b9eba
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-17 16:29:42 +0000

    change to imports and switched divs to Fragments

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Fragment } from "react";
 import Navbar from "./Navbar";
-import Home from "./Home";
+import Blogs from "./Blogs";
 import Create from "./Create";
 import About from "./About";
 import Contact from "./Contact";
+import Showcase from "./Showcase/Showcase";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
+      <Fragment className="App">
+        <Switch>
+          <Route exact path="/" component={Showcase} />
+          <Navbar />
+        </Switch>
+        <Fragment className="content">
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route path="/blogs" component={Blogs} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/create" component={Create} />
           </Switch>
-        </div>
-      </div>
+        </Fragment>
+      </Fragment>
     </Router>
   );
 }
