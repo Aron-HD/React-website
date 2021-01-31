0ec0e9041943f7b433d556d05560da058f6c1b64
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-31 14:10:15 +0000

    removed fragment

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -5,25 +5,23 @@ import Video from "../../video/showcase_video.mp4";
 
 const Showcase = () => {
   return (
-    <Fragment>
-      <section className="showcase">
-        <header>
-          <NavAlt />
-        </header>
-        <video src={Video} muted loop autoPlay />
-        <div className="overlay" />
-        <div className="container">
-          <div className="text">
-            <h3>Aron Hayes Davidson</h3>
-            <p>
-              Aspiring developer experienced in frontend and backend
-              technologies looking to change career.
-            </p>
-            <Link to="/projects">Projects</Link>
-          </div>
+    <section className="showcase">
+      <header>
+        <NavAlt />
+      </header>
+      <video src={Video} muted loop autoPlay />
+      <div className="overlay" />
+      <div className="container">
+        <div className="text">
+          <h3>Aron Hayes Davidson</h3>
+          <p>
+            Aspiring developer experienced in frontend and backend technologies
+            looking to change career.
+          </p>
+          <Link to="/projects">Projects</Link>
         </div>
-      </section>
-    </Fragment>
+      </div>
+    </section>
   );
 };
 
