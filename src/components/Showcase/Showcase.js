4fe7158f0e709bfdaf231f88223104d9fbefa5c2
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-31 11:44:05 +0000

    added showcase video

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,8 +1,7 @@
 import { Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import NavAlt from "./NavAlt";
+import Video from "../../images/showcase_video.mp4";
 
 const Showcase = () => {
   return (
@@ -11,52 +10,17 @@ const Showcase = () => {
         <header>
           <NavAlt />
         </header>
-        <video
-          src="https://www.pexels.com/video/aerial-view-of-beautiful-resort-2169880"
-          muted
-          loop
-          autoplay
-        />
+        <video src={Video} muted loop autoPlay />
         <div className="overlay" />
         <div className="container">
           <div className="text">
-            <h2>Never Stop</h2>
-            <h3>Exploring The World</h3>
+            <h3>Aron Hayes Davidson</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod.
+              Aspiring developer experienced in frontend and backend
+              technologies looking to change career.
             </p>
-            <a href="/projects">Projects</a>
+            <Link to="/projects">Projects</Link>
           </div>
-          <ul className="social">
-            <li>
-              <a href="mailto:aronhdavidson@gmail.com">
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  style={{ color: "#fff" }}
-                  size="3x"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/aron-hd">
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  style={{ color: "#fff" }}
-                  size="3x"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/aron-hd/">
-                <FontAwesomeIcon
-                  icon={faLinkedinIn}
-                  style={{ color: "#fff" }}
-                  size="3x"
-                />
-              </a>
-            </li>
-          </ul>
         </div>
       </section>
     </Fragment>
