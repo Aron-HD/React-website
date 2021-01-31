fec729fdbced9c45b550e9cdde260da6f2037379
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-31 18:42:41 +0000

    built out create component as form to create blogs

--- src/components/Blog/Create.js
+++ src/components/Blog/Create.js
@@ -1,8 +1,90 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const StyledCreate = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+  label {
+    text-align: left;
+    display: block;
+  }
+  h2 {
+    font-size: 20px;
+    color: #f1356d;
+    margin-bottom: 30px;
+  }
+  input,
+  textarea,
+  select {
+    width: 100%;
+    padding: 6px 10px;
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    box-sizing: border-box;
+    display: block;
+  }
+  button {
+    background: #f1356d;
+    color: #fff;
+    border: 0;
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("aron");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    //  prevent default refresh
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      history.push("/blogs");
+    });
+  };
   return (
-    <div className="create">
+    <StyledCreate className="create">
       <h2>Add a new blog</h2>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="aron">aron</option>
+          <option value="user">user</option>
+        </select>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
+      </form>
+    </StyledCreate>
   );
 };
 
